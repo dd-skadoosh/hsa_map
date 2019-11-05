d142c241c70248dbe8ad36781312e7b26e6640c0
Dheeraj D Suvarna <ddsuvarna25@gmail.com>
2019-11-05 16:19:55 +0530

    added interactions,age increment,enrollment month objects and extended map

---
+++ interaction.tsx
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="interaction" tilewidth="64" tileheight="64" tilecount="12" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="Other/coinBronze.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="Other/coinDiamond.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="Other/coinGold.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="Other/coinSilver.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="Other/flagBlue.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="Other/flagGreen.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="Other/flagRed.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="Other/flagYellow.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="Other/starBronze.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="Other/starDiamond.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="64" source="Other/starGold.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="64" source="Other/starSilver.png"/>
+ </tile>
+</tileset>
