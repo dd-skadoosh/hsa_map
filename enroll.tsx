5aaf42f9c232c7ed99bc73aad5fbfc060011f9a1
Dheeraj D Suvarna <ddsuvarna25@gmail.com>
2019-11-12 14:59:01 +0530

    Mostly final map

---
+++ enroll.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="enroll" tilewidth="32" tileheight="32" tilecount="3" columns="1">
+ <image source="Other/enroll.png" width="32" height="96"/>
+</tileset>
